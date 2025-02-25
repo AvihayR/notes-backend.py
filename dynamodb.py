2f0adefd0c4dad5c2e20d409ca2c10c475e1868a
AvihayR <avihayrabi@gmail.com>
2025-02-25 23:13:44 +0200

    Add read operation for a single note

--- dynamodb.py
+++ dynamodb.py
@@ -9,8 +9,32 @@ def get_notes():
         response = dynamodb.scan(
                 TableName='notes',
                 )
+        
     except ClientError as err:
         print('There was an issue getting notes, try again later', err)
-    else:
+        
+    if 'Items' in response:
         return response['Items']
+    else:
+        return f'Hmm.. seems like there\'s no notes yet.. please try again later'
 
+
+def get_note(note_id):
+    try:
+        response = dynamodb.get_item(
+            TableName='notes',
+            Key={
+                'note_id': {
+                    'S': note_id
+                }
+            })
+    
+    except ClientError as err:
+        print(f'There was an issue getting note with the ID of: {note_id}, please try again later.')
+
+    if 'Item' in response:
+        return response["Item"]
+    else:
+        return f'No note with ID of "{note_id}" found.'
+
+print(get_notes())
